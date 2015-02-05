494569234c41c5f859328d6aff463f2231dd30bb
hauge550 <hauge550@morris.umn.edu>
2015-02-05 13:24:12 -0600

    no temp array, just one data array with multiple items in a json object. woot woot! no one saw this coming. TA impressed.

--- client/views/main/main.controller.js
+++ client/views/main/main.controller.js
@@ -11,8 +11,6 @@ angular.module("appModule")
         // Normally, data like this would be stored in a database, and this controller would issue an http:get request for it.
         $scope.data = [];
 
-        $scope.temp = [];
-
         $scope.getPets = function(){
             $http.get('api/pets').success(function(pets) {
                 $scope.data = pets;
@@ -22,19 +20,19 @@ angular.module("appModule")
         $scope.getPets();
 
         $scope.addData = function() {
-            if ($scope.textField.length >= 1) {
-                $scope.temp.push({text: $scope.textField});
+            if ($scope.textField.length >= 1 && $scope.weightField >= 0) {
+                $scope.data.push({text: $scope.textField, number: $scope.weightField});
                 $scope.textField = "";
-            }
-            if ($scope.weightField.length >= 1) {
-                $scope.temp.push({text: $scope.weightField});
                 $scope.weightField = "";
             }
-            $http.post('api/pets', $scope.temp).success(function () {
+            $http.post('api/pets', $scope.data).success(function () {
                 $scope.getPets();
-
             });
-        }
+        };
+
+        //
+        //$http.post('api/pets', {text: $scope.textField}).success(function(){
+        //    $scope.getPets();
 
         $scope.removeData = function(index){
             $http.delete('/api/pets/' + $scope.data[index]._id).success(function(){
